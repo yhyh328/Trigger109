0b4bc94a892e9e2d704a3de07ef758f878f5ea10
dodokim98 <eh6848zzz@gmail.com>
2024-04-17 11:03:57 +0900

    docs : 기본 세팅 파일 업로드

---
+++ back/src/test/java/com/ssafy/c109/trigger/TriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.c109.trigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
