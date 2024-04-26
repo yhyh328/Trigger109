1aa3d6840a64016ad3f13b384ca881c2c1378fa1
kennedy1234 <kennedy1234@naver.com>
2024-04-26 09:53:08 +0900

    fix: chat dotenv

--- chat/index.js
+++ chat/index.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors:{
-        origin:"http://localhost:3000"
+        origin:"http://k10c109.p.ssafy.io:3001"
     }
 })
 
