51bf5da6eef30f523601880be768bbef1d0014d5
dodokim98 <eh6848zzz@gmail.com>
2024-04-26 11:27:25 +0900

    fix : Dockerfile & jenkinsfile

--- chat/index.js
+++ chat/index.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors:{
-        origin:"http://k10c109.p.ssafy.io:3001"
+        origin:"http://k10c109.p.ssafy.io:3002"
     }
 })
 
