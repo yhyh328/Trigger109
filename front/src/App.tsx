f257363ec6f1c9dc389baad5b9a6d1a1ab999471
kennedy1234 <kennedy1234@naver.com>
2024-05-02 09:30:51 +0900

    docs:webrtc and chat

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,12 +9,8 @@ import { MainSection5 } from './component/main/MainSection5';
 import { Footer } from './component/main/Footer';
 import { GlobalStyle } from './GlobalStyle';
 import Live from './component/live/Live';
-<<<<<<< HEAD
-import LiveDetail from './component/live/LiveDetail'
-
-=======
+import LiveDetail from './component/live/LiveDetail';
 import Guide from './component/guide/Guide';
->>>>>>> 1af4a30c01858005233829917c924ce888e79d2e
 
 const App = () => {
   return (
@@ -34,13 +30,10 @@ const App = () => {
         <Route path="/live" element={
           <Live />
         }  />
-<<<<<<< HEAD
         <Route path="/live/:userId" element={<LiveDetail />} /> // 사용자 상세 페이지 경로
-=======
         <Route path="/guide" element={
           <Guide />
         } />
->>>>>>> 1af4a30c01858005233829917c924ce888e79d2e
       </Routes>
       <Footer />
     </Router>
