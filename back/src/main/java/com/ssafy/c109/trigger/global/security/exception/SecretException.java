232f2a7cfaec7da5fd157d519cfdaaf9d9b31d5f
dodokim98 <eh6848zzz@gmail.com>
2024-05-03 11:25:45 +0900

    fix : userfiles

--- back/src/main/java/com/ssafy/c109/trigger/global/security/exception/SecretException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.c109.trigger.global.security.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class SecretException extends AuthenticationException {
-    public SecretException(String message) {
-        super(message);
-    }
-}
