02e4a653cab91639e2bb03247d50543ea97bcf70
unknown <yhyh328@naver.com>
2024-05-07 13:30:12 +0900

    fix: chat/index.js -> origin:"http://localhost:3000"

--- chat/index.js
+++ chat/index.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors:{
-        origin:"http://localhost:5001"
+        origin:"http://localhost:3000"
     }
 })
 
