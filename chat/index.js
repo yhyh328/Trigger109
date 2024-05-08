486a4752dc7560e733832c9ec8bfd2acebe3c2bb
unknown <yhyh328@naver.com>
2024-05-08 09:38:48 +0900

    fix: chat/index.js cors.origin

--- chat/index.js
+++ chat/index.js
@@ -1,16 +1,31 @@
-const { createServer } = require("https")
+const { createServer } = require("http")
 const app = require("./app")
 const { Server } = require("socket.io")
 require("dotenv").config()
 
-const httpsServer = createServer(app)
-const io = new Server(httpsServer, {
-    cors:{
-        origin:"k10c109.p.ssafy.io"
+const allowedOrigins = ["https://k10c109.p.ssafy.io", "https://trigger109.com", "https://jenkins.trigger109.com"];
+
+// const httpServer = createServer(app)
+// const io = new Server(httpServer, {
+//     cors:{
+//         origin:"k10c109.p.ssafy.io"
+//     }
+// })
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true)
+            } else {
+                callback(new Error("Origin not allowed by CORS"))
+            }
+        }
     }
 })
 
 require("./utils/io")(io)
-httpsServer.listen(process.env.PORT,() => {
+httpServer.listen(process.env.PORT,() => {
     console.log("server listening on port")
 })
