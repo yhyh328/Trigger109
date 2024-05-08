c8415532cef2b77d52e6d86b91acefa1bd83e925
unknown <yhyh328@naver.com>
2024-05-08 11:38:26 +0900

    fix: chat/Dockerfile cors_origin localhost:3000

--- chat/index.js
+++ chat/index.js
@@ -5,25 +5,25 @@ require("dotenv").config()
 
 const allowedOrigins = ["https://k10c109.p.ssafy.io", "https://trigger109.com", "https://jenkins.trigger109.com"];
 
-// const httpServer = createServer(app)
-// const io = new Server(httpServer, {
-//     cors:{
-//         origin:"k10c109.p.ssafy.io"
-//     }
-// })
-
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
-    cors: {
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true)
-            } else {
-                callback(new Error("Origin not allowed by CORS"))
-            }
-        }
+    cors:{
+        origin:"http://localhost:3000"
     }
 })
+
+// const httpServer = createServer(app)
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: function (origin, callback) {
+//             if (!origin || allowedOrigins.includes(origin)) {
+//                 callback(null, true)
+//             } else {
+//                 callback(new Error("Origin not allowed by CORS"))
+//             }
+//         }
+//     }
+// })
 
 require("./utils/io")(io)
 httpServer.listen(process.env.PORT,() => {
