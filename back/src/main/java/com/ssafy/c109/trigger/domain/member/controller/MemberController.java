69736b2b39b5aefad2a645e097cd6066679e488d
dodokim98 <eh6848zzz@gmail.com>
2024-05-08 13:43:06 +0900

    feat : s3 file upload 기능 구현

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Slf4j
