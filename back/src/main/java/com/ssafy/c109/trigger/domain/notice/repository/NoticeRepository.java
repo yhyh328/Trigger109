9c53174db3bc690fabf0da45c8e3cb502ad6c61f
dodokim98 <eh6848zzz@gmail.com>
2024-05-08 09:18:57 +0900

    feat : #485 공지사항 상세보기 api 구현

--- back/src/main/java/com/ssafy/c109/trigger/domain/notice/repository/NoticeRepository.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/notice/repository/NoticeRepository.java
@@ -4,5 +4,5 @@ import com.ssafy.c109.trigger.domain.notice.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-
+    Notice findByNoticeId(Long noticeId);
 }
