5e243492dc4c54ef22d2de42db58657cb7b175fb
dodokim98 <eh6848zzz@gmail.com>
2024-05-09 15:31:44 +0900

    test : chat/Dockerfile, Jenkinsfile, server.js

--- front/src/server.js
+++ front/src/server.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client"
-const socket = io("http://k10c109.p.ssafy.io:5001")
+// const socket = io("http://k10c109.p.ssafy.io:5001")
+const socket = io("http://chat.trigger109.com")
 export default socket;
