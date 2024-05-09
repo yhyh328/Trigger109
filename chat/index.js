30affcdc2a032ae4c0d6d5e224207e4539a9f9fb
dodokim98 <eh6848zzz@gmail.com>
2024-05-09 10:52:07 +0900

    test : chat/index.js

--- chat/index.js
+++ chat/index.js
@@ -5,25 +5,25 @@ require("dotenv").config()
 
 const allowedOrigins = ["http://k10c109.p.ssafy.io", "http://k10c109.p.ssafy.io:3000" , "http://k10c109.p.ssafy.io:3001" , "http://k10c109.p.ssafy.io:5001" ,  "http://trigger109.com", "http://jenkins.trigger109.com"];
 
-// const httpServer = createServer(app)
-// const io = new Server(httpServer, {
-//     cors:{
-//         origin:"http://localhost:3000"
-//     }
-// })
-
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
-    cors: {
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true)
-            } else {
-                callback(new Error("Origin not allowed by CORS"))
-            }
-        }
+    cors:{
+        origin:"http://k10c109.p.ssafy.io:3000"
     }
 })
+
+// const httpServer = createServer(app)
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: function (origin, callback) {
+//             if (!origin || allowedOrigins.includes(origin)) {
+//                 callback(null, true)
+//             } else {
+//                 callback(new Error("Origin not allowed by CORS"))
+//             }
+//         }
+//     }
+// })
 
 require("./utils/io")(io)
 httpServer.listen(process.env.PORT,() => {
