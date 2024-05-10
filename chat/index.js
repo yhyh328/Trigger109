94cc35ab8c817c586db784a0804918c747a6f90e
dodokim98 <eh6848zzz@gmail.com>
2024-05-10 09:29:13 +0900

    test : chat/Dockerfile

--- chat/index.js
+++ chat/index.js
@@ -3,32 +3,23 @@ const app = require("./app")
 const { Server } = require("socket.io")
 require("dotenv").config()
 
-const allowedOrigins = ["http://k10c109.p.ssafy.io", "http://k10c109.p.ssafy.io:3000" , "http://k10c109.p.ssafy.io:3001" , "http://k10c109.p.ssafy.io:5001" ,  "http://trigger109.com", "http://jenkins.trigger109.com"];
+const allowedOrigins = ["http://k10c109.p.ssafy.io", "http://k10c109.p.ssafy.io:3000", "http://k10c109.p.ssafy.io:3001", "http://k10c109.p.ssafy.io:5001", "http://trigger109.com", "http://jenkins.trigger109.com"];
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
-    cors:{
-        origin:"http://k10c109.p.ssafy.io:5002"
+    cors: {
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true)
+            } else {
+                callback(new Error("Origin not allowed by CORS"))
+            }
+        }
     }
 })
 
-// const httpServer = createServer(app)
-// const io = new Server(httpServer, {
-//     cors: {
-//         origin: function (origin, callback) {
-//             if (!origin || allowedOrigins.includes(origin)) {
-//                 callback(null, true)
-//             } else {
-//                 callback(new Error("Origin not allowed by CORS"))
-//             }
-//         }
-//     }
-// })
-
 require("./utils/io")(io)
-// httpServer.listen(process.env.PORT,() => {
-//     console.log("server listening on port")
-// })
-httpServer.listen(5002,() => {
-    console.log("server listening on port")
+
+httpServer.listen(5002, () => {
+    console.log("server listening on port 5002")
 })
