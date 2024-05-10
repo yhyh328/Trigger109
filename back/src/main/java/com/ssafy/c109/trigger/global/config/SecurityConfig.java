e10da8030a3b4cb7dbc52ea6ac24ce38b420e2df
dodokim98 <eh6848zzz@gmail.com>
2024-05-10 16:57:51 +0900

    test : back/cors 설정

--- back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/c109/trigger/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import com.ssafy.c109.trigger.global.login.handler.LoginFailureHandler;
 import com.ssafy.c109.trigger.global.login.handler.LoginSuccessHandler;
 import com.ssafy.c109.trigger.global.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,6 +27,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 import java.util.Arrays;
@@ -45,6 +47,7 @@ public class SecurityConfig {
     private final JwtService jwtService;
     private final MemberRepository memberRepository;
     private final ObjectMapper objectMapper;
+    private final  CorsFilter corsFilter;
 
 //    @Bean
 //    public CorsConfigurationSource corsConfigurationSource() {
@@ -65,6 +68,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 //                .cors()
+                .addFilter(corsFilter)
                 .csrf((AbstractHttpConfigurer::disable))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
