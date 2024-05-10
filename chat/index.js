053d01f92754714d48b6d7153f38e9919683c3e7
kennedy1234 <kennedy1234@naver.com>
2024-05-10 14:39:08 +0900

    test: CORS error

--- chat/index.js
+++ chat/index.js
@@ -3,19 +3,21 @@ const app = require("./app")
 const { Server } = require("socket.io")
 require("dotenv").config()
 
-console.log("1")
-
 const httpServer = createServer(app)
+const corsOrigin = "https://k10c109.p.ssafy.io"; // CORS origin 설정
 const io = new Server(httpServer, {
-    cors:{
-        origin:"https://k10c109.p.ssafy.io"
+    cors: {
+        origin: corsOrigin
     }
 })
-console.log("2")
-console.log("origin", origin)
+
+// Socket.IO 연결 이벤트에 로그 추가
+io.on('connection', (socket) => {
+    console.log(`Client connected from origin: ${socket.handshake.headers.origin}`);
+});
+
 require("./utils/io")(io)
-httpServer.listen(5002,() => {
-    console.log("server listening on port")
-    console.log("origin", origin)
+httpServer.listen(5002, () => {
+    console.log("Server listening on port 5002")
+    console.log("CORS origin set to", corsOrigin)
 })
-console.log("3")
