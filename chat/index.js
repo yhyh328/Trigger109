378ee16078eb24385a27e9bf6268f13296e00811
kennedy1234 <kennedy1234@naver.com>
2024-05-10 14:33:12 +0900

    test: CORS error

--- chat/index.js
+++ chat/index.js
@@ -3,38 +3,15 @@ const app = require("./app")
 const { Server } = require("socket.io")
 require("dotenv").config()
 
-// const httpServer = createServer(app)
-// const io = new Server(httpServer, {
-//     cors: {
-//         origin: function (origin, callback) {
-//             if (!origin || allowedOrigins.includes(origin)) {
-//                 callback(null, true)
-//             } else {
-//                 callback(new Error("Origin not allowed by CORS"))
-//             }
-//         }
-//     }
-// })
-
-const allowedOrigins = ["http://k10c109.p.ssafy.io", "https://k10c109.p.ssafy.io", "http://k10c109.p.ssafy.io:3001", "http://k10c109.p.ssafy.io:5001", "http://trigger109.com", "http://jenkins.trigger109.com"];
-
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
-    cors: {
-        origin: function (origin, callback) {
-            console.log('Request origin:', origin);
-            console.log('Request allowedOrigins:', allowedOrigins);
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true)
-            } else {
-                callback(new Error("Origin not allowed by CORS"))
-            }
-        }
+    cors:{
+        origin:"https://k10c109.p.ssafy.io"
     }
 })
-
+console.log("origin", origin)
 require("./utils/io")(io)
-
-httpServer.listen(5002, () => {
-    console.log("server listening on port 5002")
+httpServer.listen(5002,() => {
+    console.log("server listening on port")
+    console.log("origin", origin)
 })
