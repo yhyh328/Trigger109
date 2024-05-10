a484549423ace8bc31d943c25b05b4eac581a2a4
dodokim98 <eh6848zzz@gmail.com>
2024-05-10 14:51:01 +0900

    test : front/src/LiceDetail.tsx log

--- front/src/component/chatlist/Chat.jsx
+++ front/src/component/chatlist/Chat.jsx
@@ -4,18 +4,22 @@ import InputField from "./InputField/InputField";
 import MessageContainer from "./MessageContainer/MessageContainer"; 
 
 const Chat = () => {
+  console.log("inChat 컴포넌트");
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState('');
   const [messageList, setMessageList] = useState([]);
 
   useEffect(() => {
+    console.log("useEffect");
     socket.on('message', (message) => {
       setMessageList(prevState => [...prevState, message]);
     });
 
     const userName = prompt("당신의 이름을 입력하세요");
     socket.emit("login", userName, (res) => {
+      console.log("emit");
       if (res?.ok) {
+        console.log("res?");
         setUser(res.data);
       }
     });
@@ -24,8 +28,10 @@ const Chat = () => {
   }, []);
 
   const sendMessage = (event) => {
+    console.log("chat/sendMessage")
     event.preventDefault();
     if (message) {
+      console.log("chat/sendMessage2")
       socket.emit("sendMessage", message, (res) => {
         console.log("sendMessage res", res);
         setMessage('');
