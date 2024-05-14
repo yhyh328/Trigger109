727874677ddc8f9207fdda181bcd6a44338e6776
dodokim98 <eh6848zzz@gmail.com>
2024-05-14 16:04:44 +0900

    test : cors 설정

--- back/src/main/java/com/ssafy/c109/trigger/global/config/CorsConfig.java
+++ back/src/main/java/com/ssafy/c109/trigger/global/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 내서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
-        config.addAllowedOrigin("https://trigger109.com"); // 모든 ip에 응답을 허용하겠다.
+//        config.setAllowCredentials(true); // 내서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
+        config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용하겠다.
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용하겠다.
         config.addAllowedMethod("*"); // 모든 get, post, put, delete, patch 요청을 허용하겠다.
         config.addExposedHeader("Authorization");
