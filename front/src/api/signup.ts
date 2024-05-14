c53b9f8beddccc49763b5833603a078e2ec52064
kennedy1234 <kennedy1234@naver.com>
2024-05-14 15:30:34 +0900

    tset: member api

--- front/src/api/signup.ts
+++ front/src/api/signup.ts
@@ -12,19 +12,24 @@ export type SignUpData = {
   profileImg: File | null;
 };
 
-const url = "/api/v1/users/signup";
+const url = "api/v1/users/signup";
 
 // 회원가입 함수
 // API 호출 함수를 수정하여 FormData를 받도록 하고, 해당 함수 내에서 FormData 객체를 생성하는 부분을 제거
 export const signUp = async (formData: FormData): Promise<void> => {
     const local: AxiosInstance = localAxios();
+
+    formData.forEach((value, key) => {
+      console.log("formData", key, value);
+    });
+
     try {
       const response = await local.post(url, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          'Content-Type': 'application/json'
         }
       });
-      console.log('Response:', response.data);
+      console.log('Response Data:', response.data);
       alert('사용자 등록이 성공했습니다.');
     } catch (error) {
       console.error('Error during sign up:', error);
