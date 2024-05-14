a052133d4b36e407ba24759be4c46b771381f693
dodokim98 <eh6848zzz@gmail.com>
2024-05-14 17:23:22 +0900

    test : user 등록

--- back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/member/controller/MemberController.java
@@ -21,7 +21,7 @@ public class MemberController {
         return "jwtTest 요청 성공";
     }
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest, @RequestParam MultipartFile profileImg) {
+    public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest, @RequestParam(required = false) MultipartFile profileImg) {
         log.info("signUpRequest : " + signUpRequest);
         try {
             memberService.singUp(signUpRequest, profileImg);
