37887c9c7f2b2ba323f018c462cfd3de3a19e099
kennedy1234 <kennedy1234@naver.com>
2024-05-17 11:24:45 +0900

    feat: roomTitle

--- front/src/component/live/Live.tsx
+++ front/src/component/live/Live.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import CreateRoom from './CreateRoom';
 import { fetchUserInfo, Member } from '../../api/getuser';
+import { getRoomInfo } from '../../api/getroominfo';
+import './Live.css'
 
 interface Stream {
   userId: string;
@@ -24,7 +26,20 @@ const Live = () => {
       }
     };
 
+    const loadRoomInfo = async () => {
+      try {
+        const rooms = await getRoomInfo();
+        setStreams(rooms.map(room => ({
+          userId: room.roomId.toString(),  // `roomId`를 `userId`로 사용합니다. (필요하다면 이름을 변경하세요)
+          title: room.roomTitle            // 방제목을 `title`에 매핑
+        })));
+      } catch (error) {
+        console.error('방 정보를 불러오는 중 오류 발생:', error);
+      }
+    };
+
     loadUserInfo();
+    loadRoomInfo();
   }, []);
 
   const handleModalOpen = () => setIsModalOpen(true);
@@ -32,7 +47,7 @@ const Live = () => {
 
   const createStream = (title: string) => {
     if (!title) {
-      alert('Please enter a title for the stream.');
+      alert('스트림의 제목을 입력해주세요.');
       return;
     }
     const newUserId = `user${streams.length + 1}`; // 새로운 유저 ID 생성
@@ -42,24 +57,26 @@ const Live = () => {
   };
 
   return (
-    <div>
-      <h1>Live Streams</h1>
-      {member && <button onClick={handleModalOpen}>Create New Stream</button>}
-      {isModalOpen && (
-        <CreateRoom
-          isOpen={isModalOpen}
-          onClose={handleModalClose}
-          onCreate={createStream}
-          memberId={member ? member.memberId.toString() : ''}
-        />
-      )}
-      <ul>
-        {streams.map(stream => (
-          <li key={stream.userId}>
-            <Link to={`/live/${stream.userId}`}>{stream.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="live-stream" >
+      <div>
+        <h1>Live Streams</h1>
+        {member && <button onClick={handleModalOpen}>Create New Stream</button>}
+        {isModalOpen && (
+          <CreateRoom
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+            onCreate={createStream}
+            memberId={member?.memberId?.toString() ?? ''}
+          />
+        )}
+        <ul>
+          {streams.map(stream => (
+            <li key={stream.userId}>
+              <Link to={`/live/${stream.userId}`}>{stream.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
