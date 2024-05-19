7159c880e45e75b2f57c3010858b288c5791f226
unknown <yhyh328@naver.com>
2024-05-20 05:19:49 +0900

    test

--- front/src/api/ranking.ts
+++ front/src/api/ranking.ts
@@ -1,6 +1,5 @@
 import { AxiosInstance } from "axios";
 import { localAxios } from "../util/http-commons";
-<<<<<<< HEAD
 
 const local: AxiosInstance | undefined = localAxios();  // Good fallback, consider root cause analysis
 const url = "/api/v1/ranking";
@@ -34,70 +33,3 @@ async function fetchRankingRows(): Promise<RankingRow[]> {
 }
 
 export { fetchRankingRows }
-=======
-import * as Yup from "yup";
-
-// Establish the Axios instance
-const local: AxiosInstance | undefined = localAxios();
-const url = "/api/v1/ranking";
-
-// Define the interfaces for the API data
-interface Member {
-    memberId: number;
-    email: string;
-    nickName: string;
-    role: string;
-    level?: number;
-    createdAt: string;
-    profileImg?: string;  // Optional as per the data structure
-}
-
-interface RankingRow {
-    rankingId: number;
-    isWin: string;
-    killCnt: number;
-    death?: number;
-    createdAt: string;
-    rating?: number;
-    member: Member;
-}
-
-
-// Define the validation schema using Yup
-const rankingRowSchema = Yup.object().shape({
-    isWin: Yup.number().required(),
-    rating: Yup.number().required(),
-    member: Yup.object().shape({
-        memberId: Yup.number().required(),
-        email: Yup.string().required().email(),
-        nickName: Yup.string().required(),
-        role: Yup.string().required(),
-        level: Yup.number().nullable().notRequired(),
-        createdAt: Yup.string().required(),
-        profileImg: Yup.string().url().nullable().notRequired(),
-    }).required(),
-});
-
-// Async function to fetch and validate ranking data
-async function fetchRankingRows(): Promise<RankingRow[]> {
-    if (!local) {
-        throw new Error("Unable to fetch Axios instance.");
-    }
-    try {
-        const response = await local.get(url);
-        console.log('response.data: ', response.data)
-        return response.data
-    } catch (error: unknown) {
-        console.error("Error getting rankings:", error);
-        if (error instanceof Error) {
-            throw new Error("Error occurred while getting the rankings. " + error.message);
-        } else {
-            throw new Error("Error occurred while getting the rankings. An unknown error occurred");
-        }
-    }
-}
-
-// Export types and functions
-export type { Member, RankingRow };
-export { fetchRankingRows };
->>>>>>> b65a97152fb09e6370bd4d82de8cb1ce1e87a4b2
