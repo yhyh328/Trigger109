892441c23f79c1f9638bb72fdf593180a6777899
unknown <yhyh328@naver.com>
2024-05-19 23:26:40 +0900

    remove: back/

--- back/src/main/java/com/ssafy/c109/trigger/domain/community/mapper/CommunityMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.c109.trigger.domain.community.mapper;
-
-import org.mapstruct.factory.Mappers;
-
-public interface CommunityMapper {
-    CommunityMapper MAPPER = Mappers.getMapper(CommunityMapper.class);
-
-}
