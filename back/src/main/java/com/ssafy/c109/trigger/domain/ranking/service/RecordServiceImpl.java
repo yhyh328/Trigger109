83aee432efb0039561a78229c0ff1a04ea73aab8
dodokim98 <eh6848zzz@gmail.com>
2024-05-19 21:35:54 +0900

    fix : ranking list API

--- back/src/main/java/com/ssafy/c109/trigger/domain/ranking/service/RecordServiceImpl.java
+++ back/src/main/java/com/ssafy/c109/trigger/domain/ranking/service/RecordServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssafy.c109.trigger.domain.ranking.service;
 
+import com.ssafy.c109.trigger.domain.member.entity.Member;
+import com.ssafy.c109.trigger.domain.member.repository.MemberRepository;
 import com.ssafy.c109.trigger.domain.ranking.dto.response.GetRankingDetailListResponse;
 import com.ssafy.c109.trigger.domain.ranking.dto.response.GetRankingListResponse;
 import com.ssafy.c109.trigger.domain.ranking.entity.Ranking;
@@ -8,6 +10,8 @@ import com.ssafy.c109.trigger.domain.ranking.repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,18 +20,27 @@ import java.util.List;
 public class RecordServiceImpl implements RankingService{
 
     private final RankingRepository rankingRepository;
+    private final MemberRepository memberRepository;
     private final RankingMapper rankingMapper;
 
     @Override
     public List<GetRankingListResponse> getRankingList() {
-        try{
+        try {
             List<Ranking> getRankingdList = rankingRepository.getLatestRankingsOrderByRatingDesc();
-            List<GetRankingListResponse> getRecordListResponses = rankingMapper.toGetRankingListResponse(getRankingdList);
-            if(getRecordListResponses.isEmpty()){
+
+            List<GetRankingListResponse> getRecordListResponses = new ArrayList<>();
+
+            for (Ranking ranking : getRankingdList) {
+                GetRankingListResponse response = rankingMapper.toGetRankingListResponse(ranking);
+                getRecordListResponses.add(response);
+            }
+
+            if (getRecordListResponses.isEmpty()) {
                 throw new RuntimeException("랭킹 목록이 존재하지 않습니다.");
             }
+
             return getRecordListResponses;
-        }catch (Exception e){
+        } catch (Exception e) {
             // 예외 발생 시 로그를 남기고 예외 처리
             log.error("랭킹 목록을 불러오는 중 에러 발생: {}", e.getMessage());
             throw new RuntimeException("랭킹 목록을 불러오는 중 에러 발생했습니다.");
