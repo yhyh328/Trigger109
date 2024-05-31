5674b6a36a5d46634e0804782992772d0c678dca
unknown <yhyh328@naver.com>
2024-05-31 16:49:26 +0900

    refactor: front/.gitignore

--- front/src/component/notifications/firebase.js
+++ front/src/component/notifications/firebase.js
@@ -3,23 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from 'firebase/analytics';
 import { getMessaging, getToken } from "firebase/messaging";
 import { getFCMs, postFCM } from "../../api/fcm";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
+console.log('apiKey: ', firebaseConfig.apiKey);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -30,14 +30,12 @@ export const generateToken = async () => {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
         const FCMToken = await getToken(messaging, {
-            vapidKey: process.env.FIREBASE_VAPID_KEY
+            vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY
         });
         if (FCMToken) {
-
             try {
                 await postFCM({ fcmToken: FCMToken });
                 console.log('FCM token posted successfully');
-                
             } catch (error) {
                 console.error('Error occurred while posting the FCM token:', error);
             }
@@ -45,9 +43,8 @@ export const generateToken = async () => {
             console.error('Failed to get FCM token.');
         }
         return FCMToken;
-    }
-    else {
-        console.error('Error Occurs');
+    } else {
+        console.error('Permission not granted for Notification');
         return null;
     }
 }
